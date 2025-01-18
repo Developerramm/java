baae17985e446f8e34e3f08eadb0fe02d513c1af
ram kumar <rr697144@gmail.com>
2025-01-18 18:31:11 +0530

    variable in java

---
+++ basic/src/Variable.java
@@ -0,0 +1,35 @@
+public class Variable {
+    public static void main(String[] args) {
+        // data type
+        int age = 20;
+
+        double salary = 23432.432f;
+
+        char gender = 'M';
+
+        boolean isProgramer = true;
+
+        double pf = 243243.423;
+
+        byte range = 124;
+        System.out.println(range);
+
+        short temperature  = -200;
+        System.out.println(temperature);
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(gender);
+        System.out.println(isProgramer);
+        System.out.println(pf);
+
+        String name  = "Ram Kumar";
+        System.out.println(name);
+
+        long range1 = 4324114259070324L;
+        System.out.println(range1);
+
+        float number = 432.2432f;
+        System.out.println(number);
+    }
+}
