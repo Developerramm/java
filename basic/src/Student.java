0a810e1ca78556cf39f3728104c657cbb44889bf
ram kumar <rr697144@gmail.com>
2025-01-23 09:05:45 +0530

    java basic

---
+++ basic/src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    int rollNo = 12;
+    String name = "Ram Kumar";
+    String address = "Maniyari";
+    static int age = 30;
+
+    public  void  getSum(){
+        int a = 20;
+        int b = 30;
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+
+        //System.out.println(name);
+
+        System.out.println("Hello world");
+
+        //getSum();
+
+        System.out.println(age);
+    }
+}
