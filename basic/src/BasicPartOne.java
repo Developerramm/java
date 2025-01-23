0cfe56ee9adc0ac95a66c4ebd4b0886bf3780a5b
ram kumar <rr697144@gmail.com>
2025-01-23 09:40:59 +0530

    arithmetic operator

---
+++ basic/src/BasicPartOne.java
@@ -0,0 +1,19 @@
+public class BasicPartOne {
+    public static void main(String[] args) {
+        System.out.println("Operator,Array and Control statement");
+
+        System.out.println("java Operator");
+        System.out.println("java arithmetic operator");
+        int a = 10;
+        int b = 4;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(2 / 4);
+        System.out.println( 2 % 5);
+
+    }
+}
