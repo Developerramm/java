87870266e2b9878b0adc81182f24452c76239028
ram kumar <rr697144@gmail.com>
2025-01-27 13:58:27 +0530

    java array basic

---
+++ basic/src/class4/Array.java
@@ -0,0 +1,16 @@
+package class4;
+
+public class Array {
+    public static void main(String[] args) {
+
+        // creating array
+        int arr[] = new int[5];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        System.out.println(arr[0]);
+        System.out.println(arr.length);
+    }
+}
