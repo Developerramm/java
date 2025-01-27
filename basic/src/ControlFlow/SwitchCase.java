96c160748b869183d4c147f341d879d5271d4e5b
ram kumar <rr697144@gmail.com>
2025-01-27 14:47:53 +0530

    switch case in control flow

---
+++ basic/src/ControlFlow/SwitchCase.java
@@ -0,0 +1,38 @@
+package ControlFlow;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number b/w 1 to 7");
+        int data = input.nextInt();
+
+        switch (data){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter a valid number ");
+        }
+    }
+}
