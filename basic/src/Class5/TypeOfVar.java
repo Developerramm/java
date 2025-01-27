c7f1c73cdae5e33ecc45b1da201153d000c66aff
ram kumar <rr697144@gmail.com>
2025-01-27 21:26:29 +0530

    static variable in java

--- basic/src/Class5/TypeOfVar.java
+++ basic/src/Class5/TypeOfVar.java
@@ -10,10 +10,15 @@ public class TypeOfVar {
         obj1.name = "Maniyari";
         System.out.println(obj.name);
         System.out.println(obj1.name);
+
+        System.out.println(TypeOfVar.name);
+        TypeOfVar.name = "Ram kumar Maniyari";
+        System.out.println(TypeOfVar.name);
         //printName();
     }
 
     public void printName(){
         System.out.println("What is your name ");
+        System.out.println(TypeOfVar.name);
     }
 }
