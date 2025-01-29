e2d859072498824cf7330db8364c053ef14e1fff
ram kumar <rr697144@gmail.com>
2025-01-29 15:29:57 +0530

    LinkedHashSet and TreeSet in collection framework

--- basic/src/CollectionFrameWork/LinkedHashSetDemo.java
+++ basic/src/CollectionFrameWork/LinkedHashSetDemo.java
@@ -1,7 +1,22 @@
 package CollectionFrameWork;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class LinkedHashSetDemo {
     public static void main(String[] args) {
-        
+        //Set<Integer> st = new LinkedHashSet<>();
+        Set<Integer> st = new TreeSet<>();
+        st.add(109);
+        st.add(10);
+        st.add(10);
+        st.add(10);
+        st.add(30);
+        st.add(100);
+        st.add(20);
+        st.add(40);
+        st.add(50);
+        System.out.println(st);
     }
 }
