540c2c80d22f735396adcc09f10db616eb5ebf37
ram kumar <rr697144@gmail.com>
2025-02-02 12:33:37 +0530

    update ke try in jdbc

--- basic/src/JDBC/JDBCConnectionDemo.java
+++ basic/src/JDBC/JDBCConnectionDemo.java
@@ -1,7 +1,10 @@
 package JDBC;
 
 import java.sql.*;
+import java.util.Scanner;
+
 public class JDBCConnectionDemo {
+
     public static void  CreateConnection()throws ClassNotFoundException{
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -13,7 +16,7 @@ public class JDBCConnectionDemo {
         String url = "jdbc:mysql://localhost:3306/school";
         String username = "root";
         String password = "ram12345";
-        String query = "SELECT * FROM students;";
+        String query = "SELECT * FROM students ORDER BY id DESC;";
         try{
             Connection conn = DriverManager.getConnection(url,username,password);
             System.out.println("Database connection successful");
@@ -26,9 +29,50 @@ public class JDBCConnectionDemo {
                 double salary = result.getDouble("salary");
                 System.out.println(id + " " +  name + " " + jobTitle + " " + salary);
             }
+            result.close();
+            stm.close();
+            conn.close();
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
 
+    public  void insertData()throws ClassNotFoundException{
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Your name : ");
+        String name = input.nextLine();
+        System.out.println("Enter Your Job title : ");
+        String job_title = input.nextLine();
+        System.out.println("Enter Your Salary : ");
+        double salary = input.nextDouble();
+        System.out.println("Enter Your id : ");
+        int id = input.nextInt();
+
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        String userInput = id + ","+name + "," + job_title + "," + salary;
+        String query = "INSERT INTO students(name,job_title,salary) values (" + userInput + ");";
+        String url = "jdbc:mysql://localhost:3306/school";
+        String username = "root";
+        String password = "ram12345";
+        try{
+            Connection conn = DriverManager.getConnection(url,username,password);
+            System.out.println("Database connection successful");
+            Statement stm = conn.createStatement();
+            int result = stm.executeUpdate(query);
+            if(result > 0){
+                System.out.println("Data is added");
+            }else{
+                System.out.println("Insertion failed");
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
--- basic/src/JDBC/Main.java
+++ basic/src/JDBC/Main.java
@@ -1,12 +1,22 @@
 package JDBC;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         JDBCConnectionDemo obj = new JDBCConnectionDemo();
-        try{
+      /*  try{
             obj.CreateConnection();
         }catch (ClassNotFoundException e){
             System.out.println(e.getMessage());
+        }*/
+
+        try{
+            obj.insertData();
+        }catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
         }
+
+
     }
 }
